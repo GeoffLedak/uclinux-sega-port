5257698fe7fdc5d8c419c077c9d666b7df428c4f
Geoff Ledak <geoffledak@gmail.com>
2020-12-03 09:29:52 +0100

    it worked

--- linux-2.0.x/include/linux/compile.h
+++ linux-2.0.x/include/linux/compile.h
@@ -1,5 +1,5 @@
-#define UTS_VERSION "181 Thu Dec 3 09:17:06 CET 2020"
-#define LINUX_COMPILE_TIME "09:17:06"
+#define UTS_VERSION "185 Thu Dec 3 09:28:58 CET 2020"
+#define LINUX_COMPILE_TIME "09:28:58"
 #define LINUX_COMPILE_BY "ubuntu"
 #define LINUX_COMPILE_HOST "ubuntu-VirtualBox"
 #define LINUX_COMPILE_DOMAIN ""
